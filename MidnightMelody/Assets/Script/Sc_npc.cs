f76a495b599590f4344525747d1440cb7c4b7781
Depiliem <davew1726@gmail.com>
2025-10-14 00:48:33 +0700

    fixing collider

--- MidnightMelody/Assets/Script/Sc_npc.cs
+++ MidnightMelody/Assets/Script/Sc_npc.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Sc_npc : MonoBehaviour
 {
+    bool player_detection = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,22 @@ public class Sc_npc : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (player_detection && Input.GetKeyDown(KeyCode.F))
+        {
+            print("HI");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "hero")
+        {
+            player_detection = true;
+        }
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        player_detection = false;
     }
 }
