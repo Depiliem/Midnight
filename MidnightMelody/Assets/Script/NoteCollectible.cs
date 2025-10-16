8aa4f82d8305b1de8798e0c5b9bc1f10805998ae
Depiliem <davew1726@gmail.com>
2025-10-17 01:56:32 +0700

    collectible into clef g, adding animation

--- MidnightMelody/Assets/Script/NoteCollectible.cs
+++ MidnightMelody/Assets/Script/NoteCollectible.cs
@@ -2,6 +2,15 @@ using UnityEngine;
 
 public class NoteCollectible : MonoBehaviour
 {
+    [Header("Rotation Settings")]
+    public float rotationSpeed = 50f; // kecepatan rotasi (derajat per detik)
+
+    private void Update()
+    {
+        // 🔹 Muter di tempat pada sumbu Y
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime, Space.World);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.name == "hero")
